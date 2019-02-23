57bf97c4139e787256dcc5fdd2228fedba6ea039
arcursino <arcursino@gmail.com>
2019-02-22 23:18:14 -0300

    resolução de novos desafios

---
+++ 1007/run.sh
@@ -0,0 +1,3 @@
+python *.py < input.txt > result.txt
+diff output.txt result.txt
+rm result.txt 
---
+++ 1008/1008.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+c = float(input())
+
+salary = (b * c)
+
+print('NUMBER = {}'.format(a))
+print('SALARY = U$ {:.2f}'.format(salary))
