da22df73823082965cee889c3f47d628cb490f3b
arcursino <arcursino@gmail.com>
2019-02-28 19:36:18 -0300

    1010

---
+++ 1010/1010.py
@@ -0,0 +1,10 @@
+a, b, c = input().split()
+a, b, c = int(a), int(b), float(c)
+
+d, e, f = input().split()
+d, e, f = int(d), int(e), float(f)
+
+pagar = (b * c) + (e * f)
+
+print('VALOR A PAGAR: R$ {:.2f}'.format(pagar))
+
