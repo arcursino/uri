00e4701c30ebbefd1d6a6146bba504e1261defc4
arcursino <arcursino@gmail.com>
2019-03-01 18:40:20 -0300

    resolução do desafio 1011

---
+++ 1011/1011.py
@@ -0,0 +1,7 @@
+PI = 3.14159
+
+r = int(input())
+
+volume = (4.0/3) * PI * (r ** 3)
+
+print('VOLUME = {:.3f}'.format(volume))
---
+++ 1011/run.sh
@@ -0,0 +1,3 @@
+python3 *.py < input.txt > result.txt
+diff output.txt result.txt
+rm result.txt 
