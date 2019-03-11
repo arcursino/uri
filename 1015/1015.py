9464b022851803acfe358a0e2dcf5488190c4ef0
arcursino <arcursino@gmail.com>
2019-03-11 20:53:57 -0300

    resolução do desafio 1015

---
+++ 1015/1015.py
@@ -0,0 +1,9 @@
+import math
+
+x1, y1 = input().split()
+x1, y1 = float(x1), float(y1)
+
+x2, y2 = input().split()
+x2, y2 = float(x2), float(y2)
+
+print('{:.4f}'.format(math.sqrt((x2 - x1) ** 2.0 + (y2 - y1) ** 2.0)))
