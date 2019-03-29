3f21a086bc54a543fc4ba7a4a5057554875482c2
arcursino <arcursino@gmail.com>
2019-03-28 22:01:09 -0300

    resolução do desafio 1029

---
+++ 1029/1029.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int count;
+
+int fib(int x){
+    count++;
+    if (x == 0 || x == 1) {
+       return x;
+    } else {
+        return fib(x - 1) + fib(x -2);
+    }
+}
+
+int main(){
+    int n, x, i, result;
+
+    scanf("%d", &n);
+
+    i = 0;
+
+    while (i < n){
+        count = 0;
+
+        scanf("%d", &x);
+        result = fib(x);
+
+        printf("fib(%d) = %d calls = %d\n", x, count - 1, result);
+
+        i++;
+    }
+}
