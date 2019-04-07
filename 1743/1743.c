505a6884612e787c28927a7c9700dcdb883fdb7b
arcursino <arcursino@gmail.com>
2019-04-07 10:56:47 -0300

    correção do desafio

---
+++ 1743/1743.c
@@ -0,0 +1,29 @@
+//tags: begginer, array, control variable.
+#include <stdio.h>
+
+int main (){
+    int x[5];
+    int y[5];
+    int i, j, plugavel;
+
+    for (i = 0; i < 5; i++){
+        scanf("%d", &x[i]);
+    }
+
+    for (j = 0; j < 5; j++){
+        scanf("%d", &y[j]);
+    }
+
+    plugavel = 1;
+    for (i = 0; i < 5; i++){
+        if (x[i] == y[i]){
+            plugavel = 0;
+            break;
+        }
+    }
+    if (plugavel){
+        printf("Y\n");
+    }else{
+        printf("N\n");
+    }
+}
