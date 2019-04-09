23f782c553cd37b11d34053293da29ff7749af7e
arcursino <arcursino@gmail.com>
2019-04-08 21:45:29 -0300

    resolução do desafio 1172

---
+++ 1172/1172.c
@@ -0,0 +1,20 @@
+//tag: vetor, array
+#include <stdio.h>
+
+int main (){
+   int X[10];
+   int i;
+
+   for (i = 0; i < 10; i++){
+       scanf("%d\n", &X[i]);
+
+       if (X[i] <= 0){
+            X[i] = 1;
+       }
+
+       printf("X[%d] = %d\n", i, X[i]);
+   }
+}
+
+
+
