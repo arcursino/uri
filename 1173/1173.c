a56b63502d49894b0592b0fe06521e9865a210c8
arcursino <arcursino@gmail.com>
2019-05-13 21:47:30 -0300

    resolução do desafio 1173

---
+++ 1173/1173.c
@@ -0,0 +1,15 @@
+// tags: array
+
+#include <stdio.h>
+
+int main() {
+    int v, i;
+
+    for (i = 0; i < 10; i++) {
+        scanf("%d", &v);
+        printf("N[%d] = %d\n", i, v);
+        v *= 2;
+    }
+
+    return 0;
+}
