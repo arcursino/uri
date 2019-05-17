0d895660a61457e6b0eb29dcd87f3c7d219a2c98
arcursino <arcursino@gmail.com>
2019-05-16 22:06:15 -0300

    adiciona problema não resolvido

---
+++ 1177/1177.c
@@ -0,0 +1,17 @@
+// tags: vetor
+
+/*#include <stdio.h>
+
+int main () {
+    int t, i = 0, x;
+    int n[1000];
+
+    scanf("%d", &t);
+
+    while (i < 1000) {
+        scanf("%d\n", &n[i]);
+        i++;
+    }
+
+    return 0;
+}*/
