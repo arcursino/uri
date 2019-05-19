c683e30824b46c94cbb17bfa471a3bb9def896c9
arcursino <arcursino@gmail.com>
2019-05-19 19:06:29 -0300

    corrige cpplint

--- 1285/1285.c
+++ 1285/1285.c
@@ -8,26 +8,26 @@ int main() {
     int numero, permanece, eh_repitido, n, m, x, i, j, k;
     int numero_casa[MAX];
 
-    while(scanf("%d %d", &n, &m) == 2) {
+    while (scanf("%d %d", &n, &m) == 2) {
           permanece = 0;
-          for(i = n; i <= m; i++)  {
+          for (i = n; i <= m; i++)  {
                 eh_repitido = 0;
                 numero = 0;
                 x = i;
 
-                while(x != 0) {
+                while (x != 0) {
                       numero_casa[numero] = x % 10;
                       x /= 10;
                       numero = numero + 1;
                 }
-                for(j = 0; j < numero; j++) {
-                      for(k = j+1; k < numero; k++) {
-                            if(numero_casa[j] == numero_casa[k]) {
+                for (j = 0; j < numero; j++) {
+                      for (k = j+1; k < numero; k++) {
+                            if (numero_casa[j] == numero_casa[k]) {
                                  eh_repitido = 1;
                             }
                       }
                 }
-                if(eh_repitido == 0) {
+                if (eh_repitido == 0) {
                     permanece = permanece + 1;
                 }
           }
